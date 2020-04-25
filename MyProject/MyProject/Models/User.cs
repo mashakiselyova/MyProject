94c1dca8d8f9a41f7fcc7c1a937887a4298873e1
Maria Kiselyova <mariya.kiselyova95@gmail.com>
2020-04-25 15:47:52 +0300

    Add authentication

---
+++ MyProject/MyProject/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NewsApp.Models
+{
+    public class User : IdentityUser
+    {
+        public byte[] Avatar { get; set; }
+    }
+}
--- MyProject/MyProject/Startup.cs
+++ MyProject/MyProject/Startup.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NewsApp.Models;
 
 namespace MyProject
 {
@@ -23,6 +26,12 @@ namespace MyProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+
             services.AddControllersWithViews();
         }
 
@@ -44,6 +53,7 @@ namespace MyProject
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
