7ef31ed34e87c3ee22d47ff5304f747ab3b5a6ab
Maria Kiselyova <mariya.kiselyova95@gmail.com>
2020-05-09 20:35:30 +0300

    Add email validation

--- MyProject/MyProject/ViewModels/RegisterViewModel.cs
+++ MyProject/MyProject/ViewModels/RegisterViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MyProject.ViewModels
 {
@@ -14,6 +10,7 @@ namespace MyProject.ViewModels
 
         [Required]
         [Display(Name = "Email")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес")]
         public string Email { get; set; }
 
         [Required]
